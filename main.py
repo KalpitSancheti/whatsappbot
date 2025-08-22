d8c029085981e14ec7bebbee2df2712c6c142fbb
Kalpit Sancheti <kalpitjain3@gmail.com>
2025-08-22 11:26:24 +0530

    chahnge

--- main.py
+++ main.py
@@ -303,4 +303,4 @@ if __name__ == "__main__":
     start_clist_cron()
     start_contest_notifier()
     ping_service_b()
-    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)), debug=False)
+    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)), debug=True)
